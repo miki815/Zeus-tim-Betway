890541ec815030566331da6260749ba64311e76e
miki815 <stankrelja@gmail.com>
2022-05-09 22:58:14 +0200

    pocetna verzija sa profilom igraca i formom za deset u nizu

---
+++ faza5/aplikacija/igrac/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('<int:userId>', profil, name='profil'),
+    path('desetunizu', deset_u_nizu, name='deset'),
+]
