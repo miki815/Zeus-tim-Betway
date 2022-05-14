57efa1f6f1f5acabed2ec1cb28293dd3f336b8e0
njemanja <krivokapicnemanja0@gmail.com>
2022-05-14 20:35:01 +0200

    napravljeni modeli iz baze

--- faza5/aplikacija/igrac/urls.py
+++ faza5/aplikacija/igrac/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('<int:userId>', profil, name='profil'),
     path('desetunizu', deset_u_nizu, name='deset'),
+    path('isplati', isplati, name='isplati'),
+
 ]
--- faza5/aplikacija/igrac/views.py
+++ faza5/aplikacija/igrac/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .forms import DesetForm
+from .forms import DesetForm, IsplataForm
 
 def index(request):
     return HttpResponse("Igrac ovde")
 
 def profil(request, userId):
-    context = {}
+    status="NV"
+    stanje = 1000
+    form = IsplataForm()
+    context = {'stanje': stanje, 'form': form, 'status': status}
     return render(request, 'igrac/profil.html', context)
 
 def deset_u_nizu(request):
@@ -19,3 +22,23 @@ def deset_u_nizu(request):
         form = DesetForm()
     context = {'form': form}
     return render(request, 'igrac/desetunizu.html', context)
+
+
+
+
+def isplati(request):
+    stanje=1000
+    iznos=0
+    if (request.method == 'POST'):
+        form = IsplataForm(request.POST)
+        if (form.is_valid()):
+            iznos = form.cleaned_data['iznos']
+            if(stanje>=iznos):
+                stanje=stanje-iznos
+            context = {'form': form, 'stanje': stanje}
+            return render(request, 'igrac/profil.html', context)
+            # TODO ubaciti izbor u bazu, redirect na statistiku igre
+    else:
+        form = IsplataForm()
+    context = {'form': form, 'stanje': stanje}
+    return render(request, 'igrac/profil.html', context)
